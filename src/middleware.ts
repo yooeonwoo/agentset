003e8ca5cca3b1b18d7748fbd0a597ea90743120
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-03-11 13:31:14 +0200

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,13 @@ import { getSessionCookie } from "better-auth/cookies";
 export async function middleware(request: NextRequest) {
   const cookies = getSessionCookie(request);
 
-  // if (request.nextUrl.pathname.startsWith("/login")) {
-  //   if (cookies) {
-  //     return NextResponse.redirect(new URL("/dashboard", request.url));
-  //   }
+  if (request.nextUrl.pathname.startsWith("/login")) {
+    if (cookies) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
 
-  //   return NextResponse.next();
-  // }
+    return NextResponse.next();
+  }
 
   if (!cookies) {
     return NextResponse.redirect(new URL("/login", request.url));
