b929456128bc4a53cd041602372a559934a3bf0f
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-03-11 13:10:32 +0200

    remove workflow

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,7 +12,7 @@ export const auth = betterAuth({
     organization({
       sendInvitationEmail: async ({ email, organization, id }) => {
         await resend.emails.send({
-          from: "noreply@agentset.ai",
+          from: "Agentset.ai <noreply@agentset.ai>",
           to: email,
           subject: `Invitation to join "${organization.name}" on AgentSet.ai`,
           text: `You are invited to join ${organization.name} on AgentSet.ai. Click here to accept the invitation: ${getBaseUrl()}/accept-invitation/${id}`,
@@ -23,7 +23,7 @@ export const auth = betterAuth({
     magicLink({
       sendMagicLink: async ({ email, url }) => {
         await resend.emails.send({
-          from: "noreply@agentset.ai",
+          from: "Agentset.ai <noreply@agentset.ai>",
           to: email,
           subject: "You AgentSet login link",
           html: `<p>Click here to sign in to AgentSet.ai: <a href="${url}">here</a></p>`,
