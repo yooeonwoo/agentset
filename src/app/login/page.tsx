3816dc2357f2db9d488c896c7722d4f63dd9499a
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-03-11 13:26:14 +0200

    fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,16 @@
 import { LoginForm } from "./login-form";
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
+  const _searchParams = await searchParams;
+
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm redirectParam={searchParams.r as string | undefined} />
+        <LoginForm redirectParam={_searchParams.r as string | undefined} />
       </div>
     </div>
   );
