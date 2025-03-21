1c4e47ecaae9dd83ecbabc0524406fdff014d26f
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-03-21 03:15:24 +0200

    update

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -2,10 +2,17 @@ import { handle } from "hono/vercel";
 
 import app from "../src/app";
 
-// import { env } from "../src/env";
-
+export const runtime = "nodejs";
 // export const config = {
 //   runtime: "edge",
 // };
 
-export default handle(app);
+// import { env } from "../src/env";
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
+export const HEAD = handle(app);
+export const OPTIONS = handle(app);
