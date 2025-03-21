9e43affd0ba4299568991a858d4a2bda1542ca1b
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-03-21 06:37:45 +0200

    remove apps/api & handle api subdomain in main app

--- apps/api/src/app.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-const app = new Hono();
-
-app.get("/", (c) => {
-  return c.json({ message: "Hello Hono!" });
-});
-
-export default app;
--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,32 +1,50 @@
-import { type NextRequest, NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
-export async function middleware(request: NextRequest) {
+const redirectLogin = (request: NextRequest) =>
+  NextResponse.redirect(new URL("/login", request.url));
+
+const redirectDashboard = (request: NextRequest) =>
+  NextResponse.redirect(new URL("/dashboard", request.url));
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api/ routes
+     * 2. /_next/ (Next.js internals)
+     * 3. /_proxy/ (proxies for third-party services)
+     * 4. Metadata files: favicon.ico, sitemap.xml, robots.txt, manifest.webmanifest
+     */
+    "/((?!api/|_next/|_proxy/|favicon.ico|sitemap.xml|robots.txt|manifest.webmanifest).*)",
+  ],
+};
+
+export function middleware(request: NextRequest) {
   const cookies = getSessionCookie(request);
+  const pathname = request.nextUrl.pathname;
+  const domain = request.nextUrl.hostname;
 
-  if (request.nextUrl.pathname === "/") {
-    if (cookies) {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    }
+  // rewrite to /api
+  if (domain === "api.agentset.ai") {
+    const searchParams = request.nextUrl.searchParams.toString();
+    const searchParamsString =
+      searchParams.length > 0 ? `?${searchParams}` : "";
+    const fullPath = `${pathname}${searchParamsString}`;
 
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.rewrite(new URL(`/api${fullPath}`, request.url));
   }
 
-  if (request.nextUrl.pathname.startsWith("/login")) {
-    if (cookies) {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    }
-
-    return NextResponse.next();
+  // if the user is not logged in, and is trying to access a dashboard page, redirect to login
+  if (!cookies && (pathname.startsWith("/dashboard") || pathname === "/")) {
+    return redirectLogin(request);
   }
 
-  if (!cookies) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  // if the user is logged in, and is trying to access the login page, redirect to dashboard
+  if (cookies && (pathname.startsWith("/login") || pathname === "/")) {
+    return redirectDashboard(request);
   }
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/dashboard", "/login", "/"],
-};
