c3c8ede7eb4c20ac1481d57f357856ae73dc1240
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-03-21 02:00:29 +0200

    update api

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -1,14 +1,10 @@
 import baseConfig, { restrictEnvAccess } from "@agentset/eslint-config/base";
-import nextjsConfig from "@agentset/eslint-config/nextjs";
-import reactConfig from "@agentset/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: [".next/**"],
+    ignores: [".vercel/**"],
   },
   ...baseConfig,
-  ...reactConfig,
-  ...nextjsConfig,
   ...restrictEnvAccess,
 ];
